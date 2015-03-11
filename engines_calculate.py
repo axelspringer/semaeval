e15e066f59fbcffaf443df9002919c7a5c63f85a
amaier1 <andreas.maier@asideas.de>
2015-03-11 17:43:16 +0100

    Added engines_calculate to compute the average precision,recall and f1 score from the results of each engine for the categories PERSON, ORG, GEO, KEYWORD. Extended engines_evaluate.py to read a complete directory of files. Added utils_yaml.py with helper methods to read and write yaml without losing the ordering in the file.

---
+++ engines_calculate.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import yaml
+import utils_yaml
+
+result_dir = "result/english/"
+
+def precision(tp,tn,fp,fn):
+	return tp/float(tp + fp)
+
+def recall(tp,tn,fp,fn):
+	return tp/float(tp + fn)	
+
+def f1_score(precision, recall):
+	return 2.0 * precision * recall / float(precision + recall)
+
+def mean(l):
+	return sum(l) / float(len(l))	
+
+if __name__ == '__main__':
+
+	precisions = {}
+	recalls = {}
+	f1_scores = {}
+
+	for filename in os.listdir(result_dir):
+		if filename.endswith(".yml"):
+			with open(result_dir + filename, "r") as f:
+				data = utils_yaml.ordered_load(f, yaml.SafeLoader)
+				
+				engine = filename.split(".")[0].split("_")[1]
+
+				if engine not in precisions:
+					precisions[engine] = {}
+					recalls[engine] = {}
+					f1_scores[engine] = {}
+
+				for category in ["PERSON","GEO","ORG","KEYWORD"]:
+
+					if category not in precisions[engine]:
+						precisions[engine][category] = []
+						recalls[engine][category] = []
+						f1_scores[engine][category] = []
+					
+					tp = 0
+					tn = 0
+					fp = 0
+					fn = 0
+
+					for entities in (data[category]["detected"] + data[category]["undetected"]):
+						for key,value in entities.items():
+							if key == "TP": tp += 1
+							if key == "TN": tn += 1
+							if key == "FP": fp += 1
+							if key == "FN": fn += 1
+					try: 		
+						prec = precision(tp,tn,fp,fn)
+						rec = recall(tp,tn,fp,fn)
+						f1 = f1_score(prec, rec)
+
+						precisions[engine][category].append(prec)
+						recalls[engine][category].append(rec)
+						f1_scores[engine][category].append(f1)
+					except ZeroDivisionError:
+						pass	
+
+	results = {}
+	for engine,categories in precisions.items():
+		if engine not in results:
+			results[engine] = {}
+		for category, values in categories.items():
+			if values:
+				prec_avg = mean(values)
+				if category not in results[engine]:
+					results[engine][category] = {"precision" : prec_avg}
+				else:
+					results[engine][category]["precision"] = prec_avg
+	for engine,categories in recalls.items():
+		if engine not in results:
+			results[engine] = {}
+		for category, values in categories.items():
+			if values:
+				recall_avg = mean(values)
+				if category not in results[engine]:
+					results[engine][category] = {"recall" : recall_avg}
+				else:
+					results[engine][category]["recall"] = recall_avg
+	for engine,categories in f1_scores.items():
+		if engine not in results:
+			results[engine] = {}
+		for category, values in categories.items():
+			if values:
+				f1_avg = mean(values)
+				if category not in results[engine]:
+					results[engine][category] = {"f1_score" : f1_avg}
+				else:
+					results[engine][category]["f1_score"] = f1_avg
+
+	with open("results.yml", "w") as out: 
+		# see http://stackoverflow.com/questions/20352794/pyyaml-is-producing-undesired-python-unicode-output
+		utils_yaml.ordered_dump(results, out, Dumper=yaml.SafeDumper, default_flow_style=False, width=100, encoding="utf-8", allow_unicode=True)		
+		
+
+		
+
+
+
+
+					
+
+
+
+				
+
+
--- engines_evaluate.py
+++ engines_evaluate.py
@@ -11,39 +11,16 @@ import engines.txtrazor as textrazor
 import engines.bitext as bitext
 import engines.meaningcloud as meaningcloud
 
+import os
 import yaml
-from collections import OrderedDict
+import utils_yaml
 
-document = "article3181485.yml"
-
-example_dir = "input/english/"
-result_dir = "output/english/"
+input_dir = "input/english/"
+output_dir = "output/english/"
 
 engines = [meaningcloud, bitext, textrazor, temis, semantria, repustate, linguasys, alchemy, retresco, simple]
 
 engines = [retresco, simple]
-
-# see http://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts
-def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
-    class OrderedLoader(Loader):
-        pass
-    def construct_mapping(loader, node):
-        loader.flatten_mapping(node)
-        return object_pairs_hook(loader.construct_pairs(node))
-    OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
-    return yaml.load(stream, OrderedLoader)
-
-def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
-    class OrderedDumper(Dumper):
-        pass
-    def _dict_representer(dumper, data):
-        return dumper.represent_mapping(
-            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-            data.items())
-    OrderedDumper.add_representer(OrderedDict, _dict_representer)
-    return yaml.dump(data, stream, OrderedDumper, **kwds)
 
 def collect_results(text, engines):
 	results = {}
@@ -71,42 +48,43 @@ def collect_results(text, engines):
 
 	return (pool,results)			
 
-
 if __name__ == '__main__':
 
-	with open(example_dir + document,"r") as f:
-		data = ordered_load(f, yaml.SafeLoader)
+	for filename in os.listdir(input_dir):
+		if filename.endswith(".yml"):
+			with open(input_dir + filename,"r") as f:
+				data = utils_yaml.ordered_load(f, yaml.SafeLoader)
 
-		text = data["text"]
+				text = data["text"]
 
-		pool,results = collect_results(text, engines)
+				pool,results = collect_results(text, engines)
 
-		for engine, categories in results.items():
-			data_engine = data
-			data_engine["info"]="TP: True Positive, TN: True Negative, FP: False Positive, FN: False Negative, X: Ignored"
+				for engine, categories in results.items():
+					data_engine = data
+					data_engine["info"]="TP: True Positive, TN: True Negative, FP: False Positive, FN: False Negative, X: Ignored"
 
-			for category in ["PERSON","GEO","ORG","KEYWORD"]:
-				data_engine[category] = {}
-				
-				if category in categories:
-					entities = categories[category]	
-				else:
-					entities = []
+					for category in ["PERSON","GEO","ORG","KEYWORD"]:
+						data_engine[category] = {}
+						
+						if category in categories:
+							entities = categories[category]	
+						else:
+							entities = []
 
-				data_engine[category]["detected"] = []
-				data_engine[category]["undetected"] = []
-				
-				for entity in entities:
-					data_engine[category]["detected"].append({"FP":entity})
-				if category in pool:
-					for entity in pool[category].difference(entities):
-						data_engine[category]["undetected"].append({"FN":entity})
-							
-			document_name=document.split(".")[0] # remove suffix from document name
-			engine_name = engine.__name__.split(".")[1]
-			with open(result_dir + document_name + "_"+ engine_name + ".yml", "w") as out: 
-				# see http://stackoverflow.com/questions/20352794/pyyaml-is-producing-undesired-python-unicode-output
-				ordered_dump(data_engine, out, Dumper=yaml.SafeDumper, default_flow_style=False, width=100, encoding="utf-8", allow_unicode=True)			 		 
+						data_engine[category]["detected"] = []
+						data_engine[category]["undetected"] = []
+						
+						for entity in entities:
+							data_engine[category]["detected"].append({"FP":entity})
+						if category in pool:
+							for entity in pool[category].difference(entities):
+								data_engine[category]["undetected"].append({"FN":entity})
+									
+					filename_prefix=filename.split(".")[0] # remove suffix from filename name
+					engine_name = engine.__name__.split(".")[1]
+					with open(output_dir + filename_prefix + "_"+ engine_name + ".yml", "w") as out: 
+						# see http://stackoverflow.com/questions/20352794/pyyaml-is-producing-undesired-python-unicode-output
+						utils_yaml.ordered_dump(data_engine, out, Dumper=yaml.SafeDumper, default_flow_style=False, width=100, encoding="utf-8", allow_unicode=True)			 		 
 
 
 
