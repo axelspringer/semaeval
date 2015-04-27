998e4fa3b764c4dd2967fd97c6b546cf94c0a3bd
amaier1 <andreas.maier@asideas.de>
2015-04-27 10:42:40 +0200

    IP-1625: Activated co-reference resolution for basistech.

--- semaeval/engine/basistech/basistech.py
+++ semaeval/engine/basistech/basistech.py
@@ -20,19 +20,24 @@ def extract_entities(text, lang):
 	entities = {}
 	headers = {'content-type': 'application/json', 'accept' : 'application/json' }
 
-	# see RLP-AppDev-Guide.pdf
+	# To activate co-reference resolution one must add "NAMED_ENTITY_CHAIN_ID"
+	# to the resultTypes (see chapter 5.10, RLP-AppDev-Guide.pdf)
 	data = {'text': text,
 			'languageDetection': {"language": "UNKNOWN","strategy": "SINGLE"},
-			'resultTypes': ["DEFAULT_NAMED_ENTITY"]}
+			'resultTypes': ["DEFAULT_NAMED_ENTITY", "NAMED_ENTITY_CHAIN_ID"]}
 	rp = requests.post(host + "/rws/services/doc/processText", headers=headers, data=json.dumps(data))
 	try:
 		result = rp.json()
 
 		types = result["regions"][0]["resultAccess"]["NamedEntityTypeString"]
 		lemmas = result["regions"][0]["resultAccess"]["NormalizedNamedEntity"]
+		chain_ids = result["regions"][0]["resultAccess"]["NamedEntityChainId"]
 
-		for lemma, entity_type in zip(lemmas, types):
-			key = lemma
+		results = zip(lemmas, types, chain_ids)
+
+		for lemma, entity_type, chain_id in results:
+			# take the first element of the results triple where chain_id is pointing to.
+			key = results[chain_id][0]
 			entities[key] = convert_label(entity_type)
 	except Exception as e:
 		print "BasisTech API Error:", e
